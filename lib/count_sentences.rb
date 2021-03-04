c65d55130293ed1da2efeacf7ca178a08de4aa15
jessica you <jessicaxytan@gmail.com>
2021-03-03 23:47:18 -0500

    commit tree 6d7bb4dd29d7c6116d4e2f1c4a73fe58cc1f4d88

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #split the senses into a string
+    #put split sentences into array
+    self.split(/[[!?.]]\s/).to_a.length
+    #count number of elements in array
   end
 end
